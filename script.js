260a0896bf78fdb53a73d443ad4b5d842abd395a
Stephanie Winarto <swinarto@risd.edu>
2020-03-31 06:47:56 +0800

    March 31

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
+
+// var screen = $(window)
+
+if (window.outerWidth > 768) {
+  $(".draggable").draggable();
+  console.log("not mobile");
+
+  $('.draggable').each(function(){
+       $(this).css({"left": Math.random() * (window.outerWidth-400) , "top": Math.random() * (window.outerHeight-400)})
+  });
+}
+
+if (window.outerWidth < 768) {
+    console.log("mobile");
+
+  $("body").click(function() {
+    $(".sidenav").css("visibility", "visible");
+  });
+}
+
+
 $( function() {
-    $( ".draggable" ).draggable();
-  } );
+  $( ".draggable_origin" ).draggable();
+} );
+
+$( ".draggable" ).click( function() {
+  $(this).css('z-index', '999');
+  $( ".draggable" ).not(this).css('z-index', '0');
+
+}
+
+);
 
 
-  var player_1b = document.getElementById("1b_audio");
-  var player_1c = document.getElementById("1c_audio");
-  var player_1d = document.getElementById("1d_audio");
+  var player_1b = document.getElementById("item_1b_audio");
+  var player_1c = document.getElementById("item_1c_audio");
+  var player_1d = document.getElementById("item_1d_audio");
 
-  $(".img_1b")
+  $(".item_1b")
   .mouseenter(function() {
     player_1b.play();
   })
@@ -15,7 +44,7 @@ $( function() {
     player_1b.pause();
   });
 
-  $(".img_1c")
+  $(".item_1c")
   .mouseenter(function() {
     player_1c.play();
   })
@@ -23,7 +52,7 @@ $( function() {
     player_1c.pause();
   });
 
-  $(".img_1d")
+  $(".item_1d")
   .mouseenter(function() {
     player_1d.play();
   })

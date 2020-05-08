a2487c949ac12a6b0c73d74de1069dc691e6d71b
Stephanie Winarto <swinarto@risd.edu>
2020-05-08 19:03:55 +0800

    Video pause play

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,28 +1,138 @@
-
 function pauseAudio() {
   var sounds = document.getElementsByTagName('audio');
   for(i=0; i<sounds.length; i++) sounds[i].pause();
 };
 
 
-$( ".box-overlay" ).click(function() {
-    console.log("clicked object");
-  $(this).parent().find(".pop-up").css("display", "flex");
+$(".box-overlay.audioitem").click(function() {
+  console.log("clicked object");
+  $(this).next().css("display", "flex");
+
+  $(".close-button").click(function() {
+      console.log("clicked close button");
+    $(".pop-up").css("display", "none");
+    pauseAudio();
+  });
+
 });
+
+$(".box-overlay.videoitem").click(function() {
+  console.log("clicked object");
+  $(this).next().css("display", "flex");
+
+  var iframe = $(this).next().find("iframe");
+  var player = new Vimeo.Player(iframe);
+  player.play();
+
+  $(".close-button").click(function() {
+      console.log("clicked close button");
+    $(".pop-up").css("display", "none");
+    player.pause();
+  });
+
+});
+
+// $(".close-button").click(function() {
+//     console.log("clicked close button");
+//   $(".pop-up").css("display", "none");
+//   pauseAudio();
+// });
+
+
+
+
+// $( ".box" ).has("iframe").click(function() {
+//
+//     console.log( "has video")
+//     $(this).find(".pop-up").css("visibility", "visible");
+//         //
+//     var iframe = $(this).find("iframe");
+//     var player = new Vimeo.Player(iframe);
+//     player.play();
+//     //
+//     // $(".close-button").click(function() {
+//     //     console.log( "close");
+//     //     $(this).parents(".pop-up").css("visibility", "hidden");
+//     // });
+//     //
+//     //     var currentPopup = $(this).find(".pop-up");
+//     //     currentPopup.css("visibility", "visible")
+//     //
+//     //     currentPopup.find(".close-button").click(function() {
+//     //           console.log("clicked close button");
+//     //           currentPopup.css("visibility", "hidden");
+//     //           player.pause();
+//     //     });
+//
+// });
+
+// $( ".box" ).click(function() {
+//     // console.log( "has audio")
+//     $(this).parent().find(".pop-up").css("display", "flex");
+//
+//     // $(this).find(".close-button").click(function() {
+//     //     console.log("clicked close button");
+//     //   $('.pop-up').css("display","none");
+//     //     pauseAudio();
+//     // });
+//
+// });
+
+
+
+// $( ".box" ).has("audio").click(function() {
+//
+//     var currentPopup = $(this).find(".pop-up");
+//         currentPopup.css("visibility", "visible")
+//
+//         currentPopup.find(".close-button").click(function() {
+//               console.log("clicked close button");
+//               currentPopup.css("visibility", "hidden");
+//               pauseAudio()
+//         });
+//
+// });
+//
+// $( ".box" ).click(function() {
+//     console.log("clicked object");
+//
+//     var currentPopup = $(this).find(".pop-up");
+//     currentPopup.css("visibility", "visible")
+//
+//     var iframe = $(this).parent().find("iframe");
+//     var player = new Vimeo.Player(iframe);
+//     player.play();
+//
+//
+//     currentPopup.has("iframe").find(".close-button").click(function() {
+//           console.log("clicked close button");
+//           currentPopup.css("visibility", "hidden");
+//           player.pause();
+//     });
+//
+//     currentPopup.has("audio").find(".close-button").click(function() {
+//             console.log("clicked close button");
+//           currentPopup.css("visibility", "hidden");
+//             pauseAudio()
+//     });
+//
+//
+// });
+
+// //
+// $( ".close-button" ).click(function() {
+//     console.log("clicked close button");
+//   $(".pop-up").css("visibility", "hidden");
+//     pauseAudio();
+// });
+//
+
 
 $( ".intro-box" ).click(function() {
     console.log("clicked object");
   $(this).parent().find(".intro-pop-up").css("display", "flex");
 });
 
-
-$( ".close-button" ).click(function() {
-    console.log("clicked close button");
-  $(".pop-up" ).css("display", "none");
-  pauseAudio();
-  $('iframe').attr('src', $('iframe').attr('src'));
-
-});
 
 $( ".menu-button" ).click(function() {
     console.log("clicked object");
@@ -39,7 +149,6 @@ $( ".exit-button").click(function() {
     $(".site-title").css("display", "block");
     $(".menu-button").css("display", "block");
 });
-
 
 
 $( ".content" ).scroll(function() {
@@ -60,27 +169,25 @@ $( ".transcript-click" ).click(function() {
 });
 
 
-jQuery(function($){
-    $('.nav').bind('scroll', function(){
-        if($(this).scrollTop() + $(this).innerHeight()>=$(this)[0].scrollHeight){
-          $(".nav-wrapper").addClass("nav-wrapper-end")
-          $(".nav-wrapper").removeClass("nav-wrapper-start");}
-      });
-});
-
 $( ".nav" ).scroll(function() {
     var navpos = $( ".nav" ).scrollTop();
 
-    if(navpos <= 10){
+    if(navpos <= 50){
         console.log(navpos);
        $(".nav-wrapper").addClass("nav-wrapper-start");
-       $(".nav-wrapper").removeClass("nav-wrapper-end");}
+       $(".nav-wrapper").removeClass("nav-wrapper-end");
+       $(".scroll-down").css("display", "block");}
 
+   if(navpos >= 50){
+       console.log(navpos);
+      $(".nav-wrapper").removeClass("nav-wrapper-start");
+      $(".nav-wrapper").addClass("nav-wrapper-end");
+      $(".scroll-down").css("display", "none");}
 });
 
 $( ".nav" ).hover(function() {
-    $(".nav-wrapper").addClass("nav-wrapper-end");
     $(".nav-wrapper").addClass("nav-wrapper-start");
+    $(".nav-wrapper").addClass("nav-wrapper-end");
 }, function() {
     $(".nav-wrapper").removeClass("nav-wrapper-end");
     $(".nav-wrapper").removeClass("nav-wrapper-start");
